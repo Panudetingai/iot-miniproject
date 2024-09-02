0bcba59e0ac28f4a57a99a5f30eeee4316f541b9
panudet ingai <codedroy3@gmail.com>
2024-09-02 10:36:54 +0700

    chore: Update API route to use "iot" table instead of "tbl_Panudetingai020" table

--- src/app/api/client/route.ts
+++ src/app/api/client/route.ts
@@ -12,7 +12,7 @@ export async function POST(req: Request) {
     try {
         const { distance, duty_cycle, switchbutton} = await req.json();
         // Hash password
-        const res = await client.query('INSERT INTO "tbl_Panudetingai020" (ultrasonic_value, buzzer_value, switch_status) VALUES ($1, $2, $3) RETURNING *', [distance, duty_cycle, switchbutton]);
+        const res = await client.query('INSERT INTO "iot" (ultrasonic_value, buzzer_value, switch_status) VALUES ($1, $2, $3) RETURNING *', [distance, duty_cycle, switchbutton]);
         return new Response(JSON.stringify(res.rows[0]), {
             status: 201,
             headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' },
@@ -28,7 +28,7 @@ export async function POST(req: Request) {
 
 export async function GET(){
     try {
-        const res = await client.query('SELECT * FROM "tbl_Panudetingai020" ORDER BY id DESC LIMIT 1');
+        const res = await client.query('SELECT * FROM "iot" ORDER BY id DESC LIMIT 1');
         return new Response(JSON.stringify(res.rows), {
             status: 200,
             headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' },
@@ -45,7 +45,7 @@ export async function GET(){
 export async function PUT(req: Request){
     try {
         const {buzzerstatus} = await req.json();
-        const res = await client.query('UPDATE "tbl_Panudetingai020" SET buzzer_status = $1 RETURNING *', [buzzerstatus]);
+        const res = await client.query('UPDATE "iot" SET buzzer_status = $1 RETURNING *', [buzzerstatus]);
         return new Response(JSON.stringify(res.rows[0]), {
             status: 200,
             headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' },
--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -12,7 +12,7 @@ export async function POST(req: Request) {
     try {
         const { distance, buzzer_value, buzzer_status, swtich} = await req.json();
         // Hash password
-        const res = await client.query('INSERT INTO "tbl_Panudetingai020" (ultrasonic_value, buzzer_value, buzzer_status, switch_status) VALUES ($1, $2, $3, $4) RETURNING *', [distance, buzzer_value, buzzer_status, swtich]);
+        const res = await client.query('INSERT INTO "iot" (ultrasonic_value, buzzer_value, buzzer_status, switch_status) VALUES ($1, $2, $3, $4) RETURNING *', [distance, buzzer_value, buzzer_status, swtich]);
         return new Response(JSON.stringify(res.rows[0]), {
             status: 201,
             headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' },
@@ -28,7 +28,7 @@ export async function POST(req: Request) {
 
 export async function GET(){
     try {
-        const res = await client.query('SELECT * FROM "tbl_Panudetingai020" ORDER BY id DESC LIMIT 1');
+        const res = await client.query('SELECT * FROM "iot" ORDER BY id DESC LIMIT 1');
         return new Response(JSON.stringify(res.rows), {
             status: 200,
             headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' },
